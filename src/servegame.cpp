904bec8370475147441125c05dd80f6614d2e74a
Zentifyyy <109432346+Zentifyyy@users.noreply.github.com>
2025-08-27 23:06:11 +0100

    Added Enemy Speed
    - Now i can add difficulty!!

--- src/servegame.cpp
+++ src/servegame.cpp
@@ -1,20 +1,18 @@
 #include "raylib.h"
 #include "resource_dir.h"
-#include <iostream>
-#include <string>
+
+#include "string"
 
 class Game{
 public:
 
-	void Init() {
-
+	void Init() 
+	{
 		SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-		InitWindow(m_ScreenSize.x, m_ScreenSize.y, "Hello Raylib");
+		InitWindow(m_ScreenSize.x, m_ScreenSize.y, "Serve");
 
-		m_BallVelocity = { (float)GetRandomValue(1,1), (float)GetRandomValue(-1,1)};
-
-		//SearchAndSetResourceDir("resources");
+		m_BallVelocity = { (float)GetRandomValue(1,1), (float)GetRandomValue(-1,1) };
 
 		while (!WindowShouldClose())
 		{
@@ -26,8 +24,8 @@ public:
 
 private:
 
-	void GameLoop() {
-
+	void GameLoop() 
+	{
 		BeginDrawing();
 
 		ClearBackground(DARKGREEN);
@@ -36,7 +34,7 @@ private:
 
 		DrawBall();
 
-		DrawRectangle(m_EnemyPos.x, m_BallPos.y - (m_PlayerSize.y / 2), m_PlayerSize.x, m_PlayerSize.y, WHITE);
+		DrawEnemy();
 
 		score = std::to_string(m_EnemyScore) + " : " + std::to_string(m_PlayerScore);
 		DrawText(score.c_str(), 150, 25, 50, WHITE);
@@ -44,15 +42,27 @@ private:
 		EndDrawing();
 	}
 
-	void DrawBall() {
-		
-		DrawCircle(m_BallPos.x, m_BallPos.y, 10, WHITE);
+	void DrawEnemy() {
 
+		if (m_EnemyPos.y + (m_PlayerSize.y / 2) < m_BallPos.y ) {
+			m_EnemyPos.y += m_EnemyMoveSpeed * GetFrameTime();
+		}
+		else if (m_EnemyPos.y + (m_PlayerSize.y / 2) > m_BallPos.y) {
+			m_EnemyPos.y -= m_EnemyMoveSpeed * GetFrameTime();
+		}
+
+		DrawRectangle(m_EnemyPos.x, m_EnemyPos.y, m_PlayerSize.x, m_PlayerSize.y, WHITE);
+		
+	}
+
+	void DrawBall() 
+	{
 		m_BallPos.x += m_BallVelocity.x * m_BallMoveSpeed * GetFrameTime();
 		m_BallPos.y += m_BallVelocity.y * m_BallMoveSpeed * GetFrameTime();
 
-		if (m_BallPos.y > m_ScreenSize.y - 10 || m_BallPos.y < 0)
+		if (m_BallPos.y > m_ScreenSize.y - 10 || m_BallPos.y < 0) {
 			m_BallVelocity.y = -m_BallVelocity.y;
+		}
 
 		if (m_BallPos.x > m_ScreenSize.x) {
 			m_EnemyScore++;
@@ -69,7 +79,11 @@ private:
 			if (m_BallPos.y >= m_PlayerPos.y && m_BallPos.y <= (m_PlayerPos.y + m_PlayerSize.y)) {
 
 				m_BallVelocity.x = -m_BallVelocity.x;
-				m_BallVelocity.y += GetRandomValue(-1, 1);
+
+				if (IsKeyDown(KEY_W)) 
+					m_BallVelocity.y += -0.1f;
+				else if(IsKeyDown(KEY_S))
+					m_BallVelocity.y += 0.1f;
 
 				m_BallMoveSpeed *= 1.05f;
 			}
@@ -77,17 +91,20 @@ private:
 
 		if (m_BallPos.x <= m_EnemyPos.x + (m_PlayerSize.x + 10)) {
 
-			//if (m_BallPos.y >= m_EnemyPos.y && m_BallPos.y <= (m_EnemyPos.y + m_PlayerSize.y)) {
+			if (m_BallPos.y >= m_EnemyPos.y && m_BallPos.y <= (m_EnemyPos.y + m_PlayerSize.y)) {
 
 				m_BallVelocity.x = -m_BallVelocity.x;
 				m_BallVelocity.y += GetRandomValue(-1, 1);
 
 				m_BallMoveSpeed *= 1.05f;
-			//}
+			}
 		}
+
+		DrawCircle(m_BallPos.x, m_BallPos.y, 10, WHITE);
 	}
 
-	void DrawPlayer() {
+	void DrawPlayer() 
+	{
 
 		DrawRectangle(m_PlayerPos.x, m_PlayerPos.y, m_PlayerSize.x, m_PlayerSize.y, WHITE);
 
@@ -108,7 +125,8 @@ private:
 		}
 	}
 	
-	void ResetGame() {
+	void ResetGame() 
+	{
 
 		m_PlayerPos = { m_ScreenSize.x - 50, m_ScreenSize.y / 2 };
 
@@ -126,8 +144,11 @@ private:
 	Vector2 m_PlayerPos{ m_ScreenSize.x - 50, m_ScreenSize.y / 2 };
 
 	Vector2 m_EnemyPos{ 50, m_ScreenSize.y / 2 };
+	float m_EnemyTimer = 0;
+	float m_EnemyInterval = 0.2f;
+	int m_EnemyMoveSpeed = 400;
 
-	const int m_PlayerMoveSpeed = 500;
+	const int m_PlayerMoveSpeed = 600;
 	int m_BallMoveSpeed = 250;
 
 	int m_PlayerScore = 0, m_EnemyScore = 0;
@@ -139,4 +160,5 @@ private:
 
 	Vector2 m_BallVelocity = {};
 	Vector2 m_BallPos = {m_ScreenSize.x / 2, m_ScreenSize.y / 2 };
+	
 };
